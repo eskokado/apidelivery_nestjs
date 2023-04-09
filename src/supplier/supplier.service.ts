8e7f4953a436402f258c017ecd3bf013508fa316
eskokado <eskokado@gmail.com>
2023-04-09 10:46:14 -0300

    feat: create supplier service

---
+++ src/supplier/supplier.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class SupplierService {
+  private readonly logger = new Logger(SupplierService.name);
+
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: number): Promise<any> {
+    this.logger.log(`id: ${id}`);
+    const supplier = await this.prisma.suppliers.findFirst({
+      where: { id: id },
+    });
+    this.logger.log(`id: ${id} - ${supplier}`);
+    if (!supplier) {
+      const errorMessage = `Supplier not fount with the ID ${id}`;
+      this.logger.error(errorMessage);
+      return { error: errorMessage };
+    }
+    this.logger.log('Supplier found successfully!');
+    return supplier;
+  }
+}
