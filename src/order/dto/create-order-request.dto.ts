a164b712ea580fc7881a2cccf71c7f24dc54320d
eskokado <eskokado@gmail.com>
2023-04-09 16:38:32 -0300

    fix: remove unused dependency

--- src/order/dto/create-order-request.dto.ts
+++ src/order/dto/create-order-request.dto.ts
@@ -1,5 +1,5 @@
 import { IsNumber, IsOptional } from 'class-validator';
-import { Expose, Transform, Type } from 'class-transformer';
+import { Expose, Type } from 'class-transformer';
 
 export class CreateOrderRequestDto {
   @Expose()
