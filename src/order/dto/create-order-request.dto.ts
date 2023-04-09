a6f12cc6adf4abb5efab37e54c677dd0f310893f
eskokado <eskokado@gmail.com>
2023-04-09 07:55:42 -0300

    feat: create order request dto

---
+++ src/order/dto/create-order-request.dto.ts
@@ -0,0 +1,24 @@
+import { IsNumber, IsOptional } from 'class-validator';
+import { Expose, Transform, Type } from 'class-transformer';
+
+export class CreateOrderRequestDto {
+  @Expose()
+  @Type(() => Number)
+  clientId: number;
+  @Expose()
+  @Type(() => Number)
+  @IsNumber()
+  supplierId: number;
+  @Expose()
+  @Type(() => Number)
+  @IsNumber()
+  productId: number;
+  @Expose()
+  @Type(() => Number)
+  @IsOptional()
+  discount: number;
+  @Expose()
+  @Type(() => Number)
+  @IsNumber()
+  quantity: number;
+}
