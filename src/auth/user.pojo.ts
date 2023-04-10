ddbbae8b745e86ec557f5ef7539e087421811cfe
eskokado <eskokado@gmail.com>
2023-04-10 14:45:41 -0300

    feat: create Role enum

--- src/auth/user.pojo.ts
+++ src/auth/user.pojo.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { Role } from './role';
+import { Role } from '../enum/role.enum';
 
 @Injectable()
 export class UserPojo {
   username: string;
   password: string;
-  roles: Role[];
+  role: Role;
 
-  constructor(username: string, password: string, roles: Role[]) {
+  constructor(username: string, password: string, role: Role) {
     this.username = username;
     this.password = password;
-    this.roles = roles;
+    this.role = role;
   }
   static findByUserName(username: string) {
-    const role = new Role();
     const userUsername = 'user';
     const userPassword =
       '$2b$10$RrqlgVQTyd3IlzqL37SLj.3pPK0VJbg4fcnQBQDv9/VspjnEOEaEy';
@@ -22,13 +21,9 @@ export class UserPojo {
     const adminPassword =
       '$2b$10$lgSZobvrRPHON40.V1vdiefbsMkcGkzyuYUj84k.lO.1DKscKdc.m';
     if (username.includes(userUsername)) {
-      return new UserPojo(userUsername, userPassword, [
-        role.toEnum(role.USER.code),
-      ]);
+      return new UserPojo(userUsername, userPassword, Role.USER);
     } else if (username.includes(adminUsername)) {
-      return new UserPojo(adminUsername, adminPassword, [
-        role.toEnum(role.ADMIN.code),
-      ]);
+      return new UserPojo(adminUsername, adminPassword, Role.ADMIN);
     } else {
       return null;
     }
